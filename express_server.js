70cb71d2c47ce742a6bdc3b591e13e3f649c2b21
deepthi vuyyuru <vdeepthi.dara@gmail.com>
2023-04-18 00:30:59 -0400

    adding Routes

--- express_server.js
+++ express_server.js
@@ -6,8 +6,8 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-app.get("/", (req,res) => {
-  res.send("Hello!");
+app.get("/urls.json", (req,res) => {
+  res.json(urlDatabase);
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
