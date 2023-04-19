41eb294038d49a4ba3a686c0b16e5a39a4e4d659
deepthi vuyyuru <vdeepthi.dara@gmail.com>
2023-04-19 06:35:59 -0400

    url shortening -long url-create

--- express_server.js
+++ express_server.js
@@ -2,8 +2,17 @@ const express = require("express");
 const app = express();
 const port = 8080;
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 
+function generateRandomString() {
+  const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let result = '';
+  for (let i = 0; i < 6; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -16,6 +25,9 @@ app.get("/urls", (req,res) => {
   };
   res.render("urls_index", { urls });
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: "http://www.lighthouselabs.ca" }
   res.render("urls_show", templateVars);
@@ -24,6 +36,7 @@ app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: "http://www.google.com" }
   res.render("urls_show", templateVars);
 });
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
