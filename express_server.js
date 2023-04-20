86d6bf98c859a5725f8d3e01f1f13e5f30aecc43
deepthi vuyyuru <vdeepthi.dara@gmail.com>
2023-04-20 19:05:21 -0400

    displaying logged as username

--- express_server.js
+++ express_server.js
@@ -3,9 +3,8 @@ const app = express();
 const port = 3000;
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-//const urlsRouter = require('./routes/urls');
-//app.use(cookieParser());
-//app.use('/urls', urlsRouter);
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
 function generateRandomString() {
   const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -24,6 +23,10 @@ app.post('/login', (req, res) => {
   const { username } = req.body;
   res.cookie('username', username);
   res.redirect('/urls');
+});
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase, username: req.cookies.username };
+  res.render("urls_index", templateVars);
 });
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
