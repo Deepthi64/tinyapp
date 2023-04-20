5593309909c56a7238ae7aaca4c4e27b8390bd69
deepthi vuyyuru <vdeepthi.dara@gmail.com>
2023-04-19 22:34:11 -0400

    delete button

--- express_server.js
+++ express_server.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = 3000;
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-
 
 function generateRandomString() {
   const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -13,50 +12,48 @@ function generateRandomString() {
   }
   return result;
 }
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/urls", (req,res) => {
-  const urls = {
-    b2xVn2: "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com"
-  };
-  res.render("urls_index", { urls });
+app.get("/urls", (req, res) => {
+  res.render("urls_index", { urls: urlDatabase });
 });
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL:  urlDatabase[req.params.id] };
-  res.render("urls_show", templateVars);
-});
-//app.get("/urls/:id", (req, res) => {
-  //const templateVars = { id: req.params.id, longURL: "http://www.google.com" }
-  //res.render("urls_show", templateVars);
-//});
+
 app.post("/urls", (req, res) => {
- const longURL = req.body.longURL;
+  const longURL = req.body.longURL;
   const shortURL = generateRandomString();
-  urlDatabase[shortURL] = longURL; // Log the POST request body to the console
+  urlDatabase[shortURL] = longURL;
   res.redirect(302, `/urls/${shortURL}`);
-  //res.status(200).send(`Short URL created: ${shortURL}`); // Respond with 'Ok' (we will replace this)
 });
-app.get("/urls/:id", (req, res) => {
-  const shortURL = req.params.id;
-  const longURL = urlDatabase[shortURL];
-  res.redirect(longURL);
+
+app.post("/urls/:longURL/delete", (req, res) => {
+  const shortURL = req.params.longURL;
+  if (!urlDatabase[shortURL]) {
+    res.sendStatus(404);
+  } else {
+    delete urlDatabase[shortURL];
+    res.redirect("/urls");
+  }
 });
 app.get("/urls/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
   if (!longURL) {
     res.sendStatus(404);
   } else {
-    res.redirect(301, longURL);
+    res.redirect(longURL);
   }
 });
 
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
